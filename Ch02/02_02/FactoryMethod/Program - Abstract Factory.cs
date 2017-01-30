260682f0c6a34f5fe31ce383e8c69277956745dc
cciti <cci@teknologisk.dk>
2017-01-30 17:55:02 +0100

    Changes
    Changes

---
+++ Ch02/02_02/FactoryMethod/Program - Abstract Factory.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ICarDealer herlevDealer = DealerProvider.GetDealer("Herlev");
+			ICarDealer roskildeDealer = DealerProvider.GetDealer("Roskilde");
+
+			AbstractCar vwCarHerlev = herlevDealer.GetVWCar();
+			Console.WriteLine($"VW with name {nameof(vwCarHerlev)} costs {vwCarHerlev?.Price}");
+
+			AbstractCar audiCarHerlev = herlevDealer.GetAudiCar();
+			Console.WriteLine($"Audi with name {nameof(audiCarHerlev)} costs {audiCarHerlev?.Price}");
+
+			AbstractCar vwCarRoskilde = roskildeDealer.GetVWCar();
+			Console.WriteLine($"VW with name {nameof(vwCarRoskilde)} costs {vwCarRoskilde?.Price}");
+
+			AbstractCar audiCarRoskilde = roskildeDealer.GetAudiCar();
+			Console.WriteLine($"Audi with name {nameof(audiCarRoskilde)} costs {audiCarRoskilde?.Price}");
+
+			Console.ReadLine();
+		}
+
+	}
+
+	public abstract class AbstractCar
+	{
+		public decimal Price { get; set; }
+	}
+
+	public class RoskildeVW : AbstractCar
+	{
+		public RoskildeVW()
+		{
+			Price = 300;
+		}
+	}
+
+	public class HerlevVW : AbstractCar
+	{
+		public HerlevVW()
+		{
+			Price = 310;
+		}
+	}
+
+	public class RoskildeAudi : AbstractCar
+	{
+		public RoskildeAudi()
+		{
+			Price = 400;
+		}
+	}
+
+	public class HerlevAudi : AbstractCar
+	{
+		public HerlevAudi()
+		{
+			Price = 410;
+		}
+	}
+
+	public abstract class AbstractTire
+	{
+		public string Size { get; set; }
+	}
+
+
+	public interface ICarDealer
+	{
+		AbstractCar GetVWCar();
+		AbstractCar GetAudiCar();
+	}
+
+	public class RoskildeCarDealer : ICarDealer
+	{
+		public AbstractCar GetVWCar()
+		{
+			return new RoskildeVW();
+		}
+
+		public AbstractCar GetAudiCar()
+		{
+			return new RoskildeAudi();
+		}
+	}
+
+	public class HerlevCarDealer : ICarDealer
+	{
+		public AbstractCar GetVWCar()
+		{
+			return new HerlevVW();
+		}
+
+		public AbstractCar GetAudiCar()
+		{
+			return new HerlevAudi();
+		}
+	}
+
+	public class DealerProvider
+	{
+		public static ICarDealer GetDealer(string region)
+		{
+			switch(region)
+			{
+				case "Herlev":
+					{
+						return new HerlevCarDealer();
+					}
+				case "Roskilde":
+					{
+						return new RoskildeCarDealer();
+					}
+				default:
+					return null;
+			}
+		}
+	}
+}
---
+++ Ch02/02_02/FactoryMethod/Program - Factory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ICarDealer dealer = new RoskildeCarDealer();
+
+			AbstractCar vwCar = dealer.GetCar("VW");
+			Console.WriteLine($"vwCar with name {nameof(vwCar)} costs {vwCar?.Price}");
+
+			vwCar = dealer.GetCar("Audi");
+			Console.WriteLine($"vwCar with name {nameof(vwCar)} costs {vwCar?.Price}");
+
+			vwCar = dealer.GetCar("Mercedes");
+			Console.WriteLine($"vwCar with name {nameof(vwCar)} costs {vwCar?.Price}");
+
+			Console.ReadLine();
+		}
+
+	}
+
+	public abstract class AbstractCar
+	{
+		public decimal Price { get; set; }
+	}
+
+	public class VW:AbstractCar
+	{
+		public VW()
+		{
+			Price = 300;
+		}
+	}
+
+	public class Audi:AbstractCar
+	{
+		public Audi()
+		{
+			Price = 450;
+		}
+	}
+
+	public interface ICarDealer
+	{
+		AbstractCar GetCar(string name);
+	}
+
+	public class RoskildeCarDealer : ICarDealer
+	{
+		public AbstractCar GetCar(string name)
+		{
+			if (name == "VW")
+				return new VW();
+			else if (name == "Audi")
+				return new Audi();
+
+			return null;
+		}
+	}
+}
